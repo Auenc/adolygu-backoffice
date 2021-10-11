ded4d9c0e6499910b4ae7017054c4ca3550b1e73
Lewis <ljoncb@gmail.com>
2021-10-11 17:00:38 +0100

    Initial react-app
    removed the flutter project and used create-react-app to generate a new react project.

--- android/app/src/main/kotlin/com/example/adolygu_backoffice/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.adolygu_backoffice
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/controllers/counter_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/state_manager.dart';
-
-class CounterController extends GetxController {
-  var counter = 0.obs;
-
-  void increment() => counter++;
-}
